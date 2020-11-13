05e9070673371b69eec3a8a5e1951d5bea9c55c2
Tobias Urhaug <tobias.urhaug@friday.de>
2020-11-13 16:46:38 +0100

    force gradle to create jar again

--- src/main/java/org/learn/dockerized/UpperCaseController.java
+++ src/main/java/org/learn/dockerized/UpperCaseController.java
@@ -9,7 +9,7 @@ public class UpperCaseController {
 
     @GetMapping
     public String welcomeMessage() {
-        return "<h1>Welcome to this incredible app!</h1>";
+        return "<h1>Welcome to this incredible app :D</h1>";
     }
 
     @GetMapping("uppercase")
